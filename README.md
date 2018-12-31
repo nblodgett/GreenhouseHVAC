492e52eee450fe8ed2c1b199a97a755956a05a6f
nblodgett <nick.blodgett@yahoo.com>
2018-12-30 23:48:12 -0800

    Expanded Readme and adjusted relay logic
    * Expanded Readme
    * Flipped relay logic (High is open relay, boad LED lit on Low = closed
    relay)

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# GreenhouseHVAC
-Using an Arduino to automate heating and cooling in the greenhouse
+# Arduino Controlled Greenhouse HVAC
+
+Arduino based HVAC system for a small greenhouse. Uses household three stage evaporative cooler as air intake and greenhouse exhaust vent/fan for the cooling side. Hot side utilizes an inexpensive instant hot water heater recirculating coolant into individually controlled heated benches.
+
+## Features
+* Brings precise climate control to a hobby greenhouse while minimizing cost using easily sourced parts
+* Ability to individually control root zone temperature of individual benches allows optimization for different growing conditions
+* Adruino based system allows for expansion for marginal extra cost. Extras such as:
+   * Dalogging
+   * Supplemental light control
+   * Additional sensors such as PAR or leaf wetness
+   * Additional heat zones or radiator heater
+   * Advanced HVAC logic
+
+## Arduino Parts
+* Arduino Uno
+* Datalogger Shield
+* 16 Channel Relay Module
+* 12 Volt Power Supply (Modified computer PSU can supply 12VDC and 5VDC)
+* DS18B20 Waterproof Temperature Sensors (1 for each heating zone)
+* DHT11 Sensor (Used for greenhouse ambient temperature and humidity, additional needed for outside ambient temperature)
+
+## Installation
+* Download or clone repository
+* Extract and upload greenhouseHVAC.ino to arduino
+
+## Included Libraries
+* DHT.h
+* OneWire.h
+* DallasTemperature.h
--- greenhouseHVAC.ino
+++ greenhouseHVAC.ino
@@ -4,37 +4,44 @@
    Nick Blodgett
 */
 #include "DHT.h" //DHT11 sensor library
+//#include "OneWire.h" // Library needed for DS18B20 sensors
+//#include "DallasTemperature.h" // Library needed for DS18B20 sensors
 
 // Assign all pin I/O
-const int lowFan = 13; // Low speed cooling fan relay output
-const int medFan = 12; // Medium speed cooling fan relay output
-const int highFan = 11; // High speed cooling fan relay output
-const int coolingPump = 10; // Evaporative cooling water pump relay output
-const int actuator1 = 9; // Exhaust vent actuator 1 relay output
-const int actuator2 = 8; // Exhaust vent actuator 2 relay output
-const int pumpAlarm = 7; // Evaporative cooling low water sensor input(NC)
-const int heatPump1 = 6; // hot water pump table 1 relay output
-const int heatPump2 = 5; // hot water pump table 2 relay output
-const int heatPump3 = 4; // hot water pump table 3 relay output
-const int heatPump4 = 3; // hot water pump hydroponic reservoir relay output
-const int ghTempRh = 2; // Greenhouse temperature and RH sensor (DHT11) input
-const int ambTempRh = 1; // Ambient temperature and RH sensor (DHT11) input
-const int tableTemp = 0; // Table/hydroponic reservoir temperature inputs (DS18B20) (qty 4)
+const int lowFan = 12; // Low speed cooling fan relay output
+const int medFan = 11; // Medium speed cooling fan relay output
+const int highFan = 10; // High speed cooling fan relay output
+const int coolingPump = 9; // Evaporative cooling water pump relay output
+const int actuator1 = 8; // Exhaust vent actuator 1 relay output
+//const int heatPump1 = 7; // hot water pump table 1 relay output
+//const int heatPump2 = 6; // hot water pump table 2 relay output
+//const int heatPump3 = 5; // hot water pump table 3 relay output
+//const int heatPump4 = 4; // hot water pump hydroponic reservoir relay output
+const int ghTempRh = 3; // Greenhouse temperature and RH sensor (DHT11) input
+//const int tableTemp = 2; // Table/hydroponic reservoir temperature inputs (DS18B20) (qty 4)
 
-//DHT11 sensor
+// DHT11 sensor
 #define DHTTYPE DHT11
 DHT dht(ghTempRh, DHTTYPE);
+
+/*
+// DS18B20 Sensor
+OneWire oneWire(tableTemp);
+DallasTemperature sensors (&oneWire);
+int tableTempF;
+int tableTempC;
+*/
 
 // Greenhouse Temperature and Humidity measurements
 int temperature = 0;
 int humidity = 0;
 int setPoint = 0;
-int pumpAlarmStatus; // Status of evap cooling water pump low water sensor
 
 void setup() {
   Serial.begin(9600);
   //DHT11 Sensor Setup
   dht.begin();
+//  sensors.begin();
 
   //Setup Pin I/O
   pinMode(lowFan, OUTPUT);
@@ -42,41 +49,17 @@ void setup() {
   pinMode(highFan, OUTPUT);
   pinMode(coolingPump, OUTPUT);
   pinMode(actuator1, OUTPUT);
-  pinMode(actuator2, OUTPUT);
-  pinMode(pumpAlarm, INPUT);
-  pinMode(heatPump1, OUTPUT);
-  pinMode(heatPump2, OUTPUT);
-  pinMode(heatPump3, OUTPUT);
-  pinMode(heatPump4, OUTPUT);
+//  pinMode(heatPump1, OUTPUT);
+//  pinMode(heatPump2, OUTPUT);
+//  pinMode(heatPump3, OUTPUT);
+//  pinMode(heatPump4, OUTPUT);
   pinMode(ghTempRh, OUTPUT);
-  pinMode(ambTempRh, INPUT);
-  pinMode(tableTemp, INPUT);
-
+  //pinMode(tableTemp, INPUT);
 }
 
 void loop() {
   readDHT();
   fanSpeed();
-}
-
-void readDHT() {
-  // Wait a two seconds between measurements.
-  delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  humidity = dht.readHumidity();
-  // Read temperature as Celsius
-  temperature = dht.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  
-  // Convert temperature from C to F
-  temperature = (temperature * 9/5) + 32;
 
   // Print temperature and RH for testing
   Serial.print("Humidity: ");
@@ -85,57 +68,6 @@ void readDHT() {
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" *F ");
-}
-
-// Set Cooling Stage
-void fanSpeed() {
-
-  pumpAlarmStatus = digitalRead(pumpAlarm); // Check for low evap cooling water event
-  // Condition: temperature is less than 75 deg f, Cooling system OFF
-  if (temperature < 75) {
-    setPoint = 0;
-    digitalWrite(coolingPump, LOW); // Turn off evap cooling water pump
-    digitalWrite(lowFan, LOW);
-    digitalWrite(medFan, LOW);
-    digitalWrite(highFan, LOW);
-    digitalWrite(actuator1, LOW); // Shut roof vent
-    digitalWrite(actuator2, LOW); // or shut off exhaust fan
-  }
-  // Condition: if temperature is greater than 80 and cooling not on, turn on low
-  if (temperature > 80 && setPoint < 1) {
-    setPoint = 1;
-    // Turn on evap cooling water pump, will not turn on if low water sensor is detected
-    digitalWrite(coolingPump, pumpAlarmStatus);
-    digitalWrite(lowFan, HIGH);
-    digitalWrite(medFan, LOW);
-    digitalWrite(highFan, LOW);
-    digitalWrite(actuator1, HIGH); // Open roof vent
-    digitalWrite(actuator2, HIGH); // or energize exhaust fan
-  }
-  // Condition: If temperature is greater than 85 and cooling is off or low, turn on medium
-  if (temperature > 85 && setPoint < 2) {
-    setPoint = 2;
-    // Turn on evap cooling water pump, will not turn on if low water sensor is detected
-    digitalWrite(coolingPump, pumpAlarmStatus);
-    digitalWrite(lowFan, LOW);
-    digitalWrite(medFan, HIGH);
-    digitalWrite(highFan, LOW);
-    digitalWrite(actuator1, HIGH); // Open roof vent
-    digitalWrite(actuator2, HIGH); // or energize exhaust fan
-
-  }
-  // Condition: If temperatuer is greater than 90 and cooling is off, low, or med, turn on high
-  if (temperature > 90 && setPoint < 3) {
-    setPoint = 3;
-    // Turn on evap cooling water pump, will not turn on if low water sensor is detected
-    digitalWrite(coolingPump, pumpAlarmStatus);
-    digitalWrite(lowFan, LOW);
-    digitalWrite(medFan, LOW);
-    digitalWrite(highFan, HIGH);
-    digitalWrite(actuator1, HIGH); // Open roof vent
-    digitalWrite(actuator2, HIGH); // or energize exhaust fan
-  }
-
   //Output cooling stage result for testing
   Serial.print("Cooling Stage: ");
   Serial.println(setPoint);
@@ -145,9 +77,68 @@ void fanSpeed() {
   Serial.println(digitalRead(medFan));
   Serial.print("High: ");
   Serial.println(digitalRead(highFan));
-  Serial.print("coolingPump: ");
+  // Print cooling pump status for testing
+  Serial.print("Cooling Pump: ");
   Serial.println(digitalRead(coolingPump));
-  Serial.print("Pump Alarm: ");
-  Serial.println(pumpAlarmStatus);  
+  // Print exhaust status for testing
+  Serial.print("Exhaust: ");
+  Serial.println(digitalRead(actuator1));
+}
 
+void readDHT() {
+  // Wait a two seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  humidity = dht.readHumidity();
+  // Read temperature as Fahrenheit 
+  temperature = dht.readTemperature(true);
+}
+
+
+
+// Set Cooling Stage
+void fanSpeed() {
+  //If temperature sensor does not read, exit cooling function without changing setpoint
+  if (temperature == 0 && humidity == 0) {
+    return;
+  }
+  
+  // Condition: temperature is less than 75 deg f, Cooling system OFF
+  if (temperature < 75) {
+    setPoint = 0;
+    digitalWrite(lowFan, HIGH);
+    digitalWrite(medFan, HIGH);
+    digitalWrite(highFan, HIGH);
+    digitalWrite(coolingPump, HIGH); // Turn cooling pump OFF
+    digitalWrite(actuator1, HIGH); // Close vent or turn OFF exhaust fan
+  }
+  // Condition: if temperature is greater than 80 and cooling not on, turn on low
+  if (temperature > 80 && setPoint < 1) {
+    setPoint = 1;
+    digitalWrite(lowFan, LOW);
+    digitalWrite(medFan, HIGH);
+    digitalWrite(highFan, HIGH);
+    digitalWrite(coolingPump, LOW); // Turn cooling pump ON
+    digitalWrite(actuator1, LOW); // Open vent or turn ON exhaust fan   
+  }
+  // Condition: If temperature is greater than 85 and cooling is off or low, turn on medium
+  if (temperature > 85 && setPoint < 2) {
+    setPoint = 2;
+    digitalWrite(lowFan, HIGH);
+    digitalWrite(medFan, LOW);
+    digitalWrite(highFan, HIGH);
+    digitalWrite(coolingPump, LOW); // Turn cooling pump ON
+    digitalWrite(actuator1, LOW); // Open vent or turn ON exhaust fan    
+  }
+  // Condition: If temperature is greater than 90 and cooling is off, low, or med, turn on high
+  if (temperature > 90 && setPoint < 3) {
+    setPoint = 3;
+    digitalWrite(lowFan, HIGH);
+    digitalWrite(medFan, HIGH);
+    digitalWrite(highFan, LOW);
+    digitalWrite(coolingPump, LOW); // Turn cooling pump ON
+    digitalWrite(actuator1, LOW); // Open vent or turn ON exhaust fan
+  }
 }
